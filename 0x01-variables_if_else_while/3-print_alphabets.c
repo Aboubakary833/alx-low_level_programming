5d8f02f17a7f649566dd3bc1eed84f8be4777487
Aboubakary833 <58236609+Aboubakary833@users.noreply.github.com>
2023-02-17 23:26:31 +0000

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c_in_lc, c_in_uc;
+
+for (c_in_lc = 'a'; c_in_lc <= 'z'; c_in_lc++)
+{
+putchar(c_in_lc);
+}
+for(c_in_uc = 'A'; c_in_uc <= 'Z'; c_in_uc++)
+{
+    putchar(c_in_uc);
+}
+putchar('\n');
+
+return (0);
+}
