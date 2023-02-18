1811a02e717c136061caaf9d47f833c77c82d953
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-18 19:15:05 +0000

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-    int n, m;
+int n, m;
 
-    for (n = 0; n <= 9; n++)
-    {
-    for(m = n + 1; m <= 9; m++)
-    {
-    putchar(n + '0');
-    putchar(m + '0');
-    if(n != 8)
-    {
-    putchar(',');
-    putchar(' ');
-    }
-    }
-    }
-    putchar('\n');
+for (n = 0; n <= 9; n++)
+{
+for (m = n + 1; m <= 9; m++)
+{
+putchar(n + '0');
+putchar(m + '0');
+if (n != 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
