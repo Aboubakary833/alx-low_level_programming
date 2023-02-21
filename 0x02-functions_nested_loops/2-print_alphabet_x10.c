e9c2c2044b91855900da7359fdc216e130ad2067
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-21 21:41:39 +0000

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,18 @@
 
 void print_alphabet_x10(void)
 {
-    int n;
-    char l;
+int n;
+char l;
 
-    for (n = 0; n < 10; n++)
-    {
-        for (l = 'a'; l <= 'z'; l++)
-        {
-            _putchar(l);
-            if (l == 'z')
-            {
-                _putchar('\n');
-            }
-        }
-    }
+for (n = 0; n < 10; n++)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+if (l == 'z')
+{
+_putchar('\n');
+}
+}
+}
 }
