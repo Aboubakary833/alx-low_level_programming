2bef8bddcb7dcfc150bb4238a2480e7109cd2e22
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-21 21:40:30 +0000

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+    int n;
+    char l;
+
+    for (n = 0; n < 10; n++)
+    {
+        for (l = 'a'; l <= 'z'; l++)
+        {
+            _putchar(l);
+            if (l == 'z')
+            {
+                _putchar('\n');
+            }
+        }
+    }
+}
