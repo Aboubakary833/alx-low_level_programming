c383004ff9a1632dcade4f2676fd100f1e07b475
Aboubakary833 <58236609+Aboubakary833@users.noreply.github.com>
2023-02-22 01:50:18 +0000

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addition two int
+ * @a: first int
+ * @b: second int
+ * Return: On success the sum.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
