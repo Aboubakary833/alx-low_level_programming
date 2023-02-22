f4666dc73a86f08fa164ef2c0b158a1a7c7bbb96
Aboubakary833 <58236609+Aboubakary833@users.noreply.github.com>
2023-02-22 01:29:33 +0000

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - check if c is a letter
+ * @c: The value to check
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void times_table(void)
+{
+int n, m;
+for(n = 0; n < 10; n++)
+{
+for(m = 0; m < 10; m++)
+{
+if((m * n) < 10)
+{
+_putchar((m * n) + '0');
+}
+else
+{
+_putchar((m * n) / 10 + '0');
+_putchar((m * n) % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
