53426e7c210b187086dcbffc121e14458e02ed45
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-22 17:28:56 +0000

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,38 +2,38 @@
 
 void print_times_table(int n)
 {
-    int m, o;
-    if (n > 0 && n <= 15)
-    {
-        for (m = 0; m <= n; m++)
-        {
-            for (o = 0; o <= n; o++)
-            {
-                if (o != 0)
-                {
-                    if ((m * o) < 10)
-                    {
-                        printf("   ");
-                    }
-                    else if ((m * o) < 100)
-                    {
-                        printf("  ");
-                    }
-                    else if ((m * o) < 1000)
-                    {
-                        printf(" ");
-                    }
-                }
-                if (o != n)
-                {
-                    printf("%d,", (m * o));
-                }
-                else
-                {
-                    printf("%d", (m * o));
-                }
-            }
-        putchar('\n');
-        }
-    }
+int m, o;
+if (n >= 0 && n <= 15)
+{
+for (m = 0; m <= n; m++)
+{
+for (o = 0; o <= n; o++)
+{
+if (o != 0)
+{
+if ((m * o) < 10)
+{
+printf("   ");
+}
+else if ((m * o) < 100)
+{
+printf("  ");
+}
+else if ((m * o) < 1000)
+{
+printf(" ");
+}
+}
+if (o != n)
+{
+printf("%d,", (m * o));
+}
+else
+{
+printf("%d", (m * o));
+}
+}
+putchar('\n');
+}
+}
 }
