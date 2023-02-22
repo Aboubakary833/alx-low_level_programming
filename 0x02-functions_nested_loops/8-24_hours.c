f04da71a04bd1e4d8014e5fc7d5c8bd69170b960
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-22 00:30:03 +0000

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,44 +8,44 @@
  */
 void jack_bauer(void)
 {
-    int h_f, h_l, m_f, m_l;
-    for (h_f = 0; h_f <= 2; h_f++)
-    {
-        if (h_f < 2)
-        {
-            for (h_l = 0; h_l < 10; h_l++)
-            {
-                for (m_f = 0; m_f < 6; m_f++)
-                {
-                    for (m_l = 0; m_l < 10; m_l++)
-                    {
-                        _putchar(h_f + '0');
-                        _putchar(h_l + '0');
-                        _putchar(':');
-                        _putchar(m_f + '0');
-                        _putchar(m_l + '0');
-                         _putchar('\n');
-                    }
-                }
-            }
-        }
-        else
-        {
-            for (h_l = 0; h_l < 4; h_l++)
-            {
-                for (m_f = 0; m_f < 6; m_f++)
-                {
-                    for (m_l = 0; m_l < 10; m_l++)
-                    {
-                        _putchar(h_f + '0');
-                        _putchar(h_l + '0');
-                        _putchar(':');
-                        _putchar(m_f + '0');
-                        _putchar(m_l + '0');
-                         _putchar('\n');
-                    }
-                }
-            }
-        }
-    }
+int h_f, h_l, m_f, m_l;
+for (h_f = 0; h_f <= 2; h_f++)
+{
+if (h_f < 2)
+{
+for (h_l = 0; h_l < 10; h_l++)
+{
+for (m_f = 0; m_f < 6; m_f++)
+{
+for (m_l = 0; m_l < 10; m_l++)
+{
+_putchar(h_f + '0');
+_putchar(h_l + '0');
+_putchar(':');
+_putchar(m_f + '0');
+_putchar(m_l + '0');
+_putchar('\n');
+}
+}
+}
+}
+else
+{
+for (h_l = 0; h_l < 4; h_l++)
+{
+for (m_f = 0; m_f < 6; m_f++)
+{
+for (m_l = 0; m_l < 10; m_l++)
+{
+_putchar(h_f + '0');
+_putchar(h_l + '0');
+_putchar(':');
+_putchar(m_f + '0');
+_putchar(m_l + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
 }
