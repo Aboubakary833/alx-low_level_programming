5fdae83022e136d3c0e8b323f8e6ab784136a414
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-23 19:43:43 +0000

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+if (size > 0)
+{
+int i, j, o;
+for (i = 1; i <= size; i++)
+{
+for (o = 0; o < (size - i); o++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
