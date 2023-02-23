c315c525305915b8c0030beb5fad3ae09ba0cd79
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-23 16:38:57 +0000

    done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - Check if a character is a digit
+ * @c: Element to check
+ * Return: On success 1.
+ * Else return 0.
+*/
+int _isdigit(int c)
+{
+return (isdigit(c) ? 1 : 0);
+}
