4cd6708d4d27cb1b79ce2db75b9540567d264983
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-27 23:53:08 +0000

    done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+/**
+ * _strcpy - Copy a string to another string variable
+ * @dest: Variable to copy to
+ * @src: String to copy
+ * Return: *char
+*/
+char *_strcpy(char *dest, char *src)
+{
+int n = 0;
+for (; src[n] != 0; n++)
+{
+*(dest + n) = src[n];
+}
+return (dest);
+}
