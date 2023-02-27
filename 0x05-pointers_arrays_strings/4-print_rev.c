210ef998e08435629978c3dc70187710e999ef4a
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-27 20:22:08 +0000

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,7 @@ void print_rev(char *s)
     int c = strlen(s);
     int cc = c;
     int i = 0;
-    char *r;
-    for (; i < c; i++)
-    {
-    r[i] = s[cc - 1];
-    cc--;
-    }
+    char *r = s;
+    
     _puts(r);
 }
