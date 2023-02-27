137d61e7456dc615abd59db3505714adb22faad2
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-27 23:18:29 +0000

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,9 @@
 void puts_half(char *str)
 {
 int l = strlen(str), n = l / 2;
-if (l % 2 == 0)
-{
-for (; n < l - 1; n++)
-{
-_putchar(str[n]);
-}
-}
-else
-{
 for (; n < l; n++)
 {
 _putchar(str[n]);
-}
 }
 _putchar('\n');
 }
