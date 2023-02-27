c27badbcb9428320203e663148cc9054f17fc4d4
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-02-27 19:44:27 +0000

    done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - Swap two integer values
+ * @a: First integer
+ * @b: Second integer
+ * Return: void
+*/
+void swap_int(int *a, int *b)
+{
+int aVal = *a;
+int bVal = *b;
+*a = bVal;
+*b = *a;
+}
