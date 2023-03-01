d4d68c4b50d076a8a5dcf4a70127aa33b26b1de5
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-01 17:30:24 +0000

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strncpy - Copy a string
+ * @dest: Destination
+ * @src: Source
+ * @n: The size
+ * Return: char *
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+return (strncpy(dest, src, n));
+}
