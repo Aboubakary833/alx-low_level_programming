b4dcdac52deaada64b371602e28b33a42f75fe88
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-01 21:08:25 +0000

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,12 @@
 char *leet(char *str)
 {
 char *letters = "aAeEoOtTlL";
-int digits[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
-int c = 0, l = strlen(letters), rl, i;
-char *r;
+int digits[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1}, c = 0;
 for (; str[c] != '\0'; c++)
 {
 if (strchr(letters, str[c]))
 {
-r = strchr(letters, str[c]);
-rl = strlen(r);
-i = l - rl;
-*(str + c) = (digits[i] + '0');
+*(str + c) = (digits[strlen(letters) - strlen(strchr(letters, str[c]))] + '0');
 }
 }
 return (str);
