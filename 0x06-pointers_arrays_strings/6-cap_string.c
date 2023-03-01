3c27828ca2f860cab66489b8056ac771f517e867
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-01 19:11:42 +0000

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * cap_string - Capitalize a string
+ * @str: String to capitalize
+ * Return: char *
+*/
+char *cap_string(char *str)
+{
+int l = strlen(str), c = 0, nc;
+for (; c < l; c++)
+{
+if (str[c] == '\t' || str[c] == '\n' || str[c] == '.')
+{
+nc = c + 1;
+*(str + nc) = toupper(str[nc]);
+}
+}
+return str;
+}
