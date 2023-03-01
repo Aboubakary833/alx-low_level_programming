b8f75c10cc6bcc9adbd2fbc720cbafaa86615782
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-01 19:43:56 +0000

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 int l = strlen(str), c = 0, nc;
 for (; c < l; c++)
 {
-if (strchr(" \t\n,;.!?\"(){}\0", str[c]))
+if (strchr(" \t\n,;.!?\"(){}\0\r\f\v", str[c]))
 {
 nc = c + 1;
 *(str + nc) = toupper(str[nc]);
