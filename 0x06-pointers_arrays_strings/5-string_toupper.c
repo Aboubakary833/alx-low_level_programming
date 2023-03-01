f7cc16cee6e9cefe11af378db0fd613556cccfd5
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-01 18:36:36 +0000

    done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * Transform a string to uppercase
+ * @str: String to transform
+ * Return: *char
+*/
+char *string_toupper(char *str)
+{
+int l = strlen(str), c = 0;
+char *r = str;
+for (; c < l; c++)
+{
+*(r + c) = toupper(str[c]);
+}
+return r;
+}
