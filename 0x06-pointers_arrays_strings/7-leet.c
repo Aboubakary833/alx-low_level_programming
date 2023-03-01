17782a559bce0679b39146028260ade590f656ae
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-01 20:56:32 +0000

    done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+/**
+ * leet - Encode a string
+ * @str: String to encode
+ * Return: char *
+*/
+char *leet(char *str)
+{
+char *letters = "aAeEoOtTlL";
+int digits[] = {4, 4, 3, 3, 0, 0, 7, 7};
+int c = 0, l = strlen(letters), rl, i;
+char *r;
+for (; str[c] != '\0'; c++)
+{
+if ((r = strchr(letters, str[c])))
+{
+rl = strlen(r);
+i = l - rl;
+*(str + c) = (digits[i] + '0');
+}
+}
+return (str);
+}
