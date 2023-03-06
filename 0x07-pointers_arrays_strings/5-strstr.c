1aca97212aaa0fd2399017ed93cd6884521c4bac
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-06 19:08:56 +0000

    done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strstr - Search a string in a string
+ * @haystack: The string to search inside
+ * @needle: The string to search
+ * Return: char
+*/
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}
