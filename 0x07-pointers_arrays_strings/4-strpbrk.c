dc65d66b74a3f0a9f9c83055cbaf78593d19ea41
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-06 18:11:17 +0000

    done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strpbrk - Search a set of char in a string
+ * @s: The string
+ * @accept: The set of char
+ * Return: char
+*/
+char *_strpbrk(char *s, char *accept)
+{
+return (strpbrk(s, accept));
+}
