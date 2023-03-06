b7fb165555c374b0e6f59f02a0268648a582cfcf
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-06 14:43:06 -0800

    done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,9 +3,20 @@
 /**
  * print_diagsums - Print diagonal sum
  * @a: The int array-pointer
- * @size: 
+ * @size: The size
+ * Return: void
 */
 void print_diagsums(int *a, int size)
 {
-
+int s = (size * size), i, j;
+int firstTotal = 0, secondTotal = 0;
+for (i = 0; i < s; i += size)
+{
+firstTotal += *(a + i);
 }
+for (j = (s - 1); j >= 0; j -= size)
+{
+secondTotal += *( a + j);
+}
+}
+
