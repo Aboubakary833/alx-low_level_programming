5a18a0dd8b88388f57d6764207e7c5c41f4a9bf0
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-08 22:21:18 +0000

    done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print recursively a string in reverse
+ * @s: The string
+ * Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+char substr[1000];
+int l = strlen(s), pos = l - 1;
+if (l > 0)
+{
+_putchar(s[pos]);
+strncpy(substr, s, (l - 1));
+_print_rev_recursion(substr);
+}
+else
+{
+_putchar('\n');
+}
+}
