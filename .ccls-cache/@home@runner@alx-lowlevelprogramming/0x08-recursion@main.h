9524a723ea77e2b421da25b2ce2b0c79d8d14e9b
Aboubakary Cissé <58236609+Aboubakary833@users.noreply.github.com>
2023-03-09 02:47:20 +0000

    done

--- .ccls-cache/@home@runner@alx-lowlevelprogramming/0x08-recursion@main.h
+++ .ccls-cache/@home@runner@alx-lowlevelprogramming/0x08-recursion@main.h
@@ -7,6 +7,6 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-return (x * _pow_recursion(x, (y - 1)));
+int _sqrt_recursion(int n);
 
 #endif
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+/**
+* is_prime_number - check if prime
+* @n: The int
+* Return: int
+*/
+int is_prime_number(int n)
+{
+return (((n / 1) == 0 && (n / n) == 0) ? 1 : 0);
+}
