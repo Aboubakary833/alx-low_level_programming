54cd57813f79c809e34346eb759e9ff58cd30bfc
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-10 23:22:20 +0000

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print arguments length
+ * @argc: The arguments array size
+ * @argv: The arguments array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
