22e2636cab49f4f45f7ce58a3b8eccf056678d39
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-11 00:25:48 +0000

    done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Mutiply two int
+ * @argc: The arguments array size
+ * @argv: The arguments array
+ * Return: 0 on Success or 1 on Error.
+ */
+int main(int argc, char *argv[])
+{
+int result;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+}
