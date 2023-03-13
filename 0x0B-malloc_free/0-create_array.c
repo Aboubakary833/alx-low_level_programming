7bd8d230f3886ccd901671004b48697c78ed5918
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-13 21:09:10 +0000

    done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * create_array - Create an array of character
+ * @size: The size of the array
+ * @c: The character
+ * Return: String on Success or NULL on Error
+*/
+char *create_array(unsigned int size, char c)
+{
+int i, c_size = size ? size : 0;
+char *s = malloc(c_size * sizeof(char) + 1);
+if (c_size == 0 || !s)
+return (NULL);
+for (; i < c_size; i++)
+{
+s[i] = c;
+}
+s[i] = '\0';
+
+return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+## This project is about malloc and free
+
+### Malloc or dynamic memory allocation
+
+### Malloc or dynamic memory allocation
