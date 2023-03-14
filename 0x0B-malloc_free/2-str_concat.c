675d21931596621e72f1d705114d7cb05cc84a3e
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-14 18:11:53 +0000

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,13 +19,13 @@ final_str = malloc((len + 1) * sizeof(char));
 if (final_str == NULL)
 return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++)
+for (i = 0; cp_s1[i] != '\0'; i++)
 {
-final_str[i] = s1[i];
+final_str[i] = cp_s1[i];
 }
-for (j = 0; s2[j] != '\0'; j++)
+for (j = 0; cp_s2[j] != '\0'; j++)
 {
-final_str[i] = s2[j];
+final_str[i] = cp_s2[j];
 i++;
 }
 final_str[i] = '\0';
