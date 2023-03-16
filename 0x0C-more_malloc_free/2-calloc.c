52d11b9b1ccca3a2bf4037c599354e0c698eaac5
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-16 21:54:53 +0000

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int i = 0;
-int total = nmemb;
+int total = nmemb * size;
 char *str_ptr;
 void *ptr;
 if (nmemb == 0 || size == 0)
 return (NULL);
-ptr = malloc(nmemb * size);
+ptr = malloc(total);
 if (ptr == NULL)
 {
 free(ptr);
