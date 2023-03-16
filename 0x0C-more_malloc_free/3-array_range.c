03c3bb91dc1dbe49fd5f6b71d88ecb0265643c8b
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-16 22:05:42 +0000

    done

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,9 @@ int *range, size, i;
 if (min > max)
 return (NULL);
 size = (max - min) + 1;
-range = (int)malloc(sizeof(int) * size);
+range = (int *)malloc(sizeof(int) * size);
+if (range == NULL)
+return (NULL);
 for (i = 0; i < size; i++)
 {
 range[i] = min;
