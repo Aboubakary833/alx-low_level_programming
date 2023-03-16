ca0866723a42b8e059ff34e08c4c74436c65a0ea
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-16 20:51:25 +0000

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdlib.h>
 #include "main.h"
 
@@ -7,26 +6,31 @@
  * @s1: First string
  * @s2: Second string
  * @n: Size
+ * Return: char-pointer on Success or NULL
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *cp_1, *cp_2, *str;
 int i, j, size, length;
+char *str;
 size = (int) n;
 
-cp_1 = s1 == NULL ? "" : s1;
-cp_2 = s2 == NULL ? "" : s2;
+s1 = s1 == NULL ? "" : s1;
+s2 = s2 == NULL ? "" : s2;
 
 if (size < 0)
 return (NULL);
-length = size + strlen(cp_1);
+if (size >= _strlen_recursion(s2))
+{
+size = _strlen_recursion(s2);
+}
+length = size + _strlen_recursion(s1);
 str = (char *) malloc(length *sizeof(char *));
 if (str == NULL)
 return (NULL);
-for (i = 0; cp_1[i] != '\0'; i++)
-str[i] = cp_1[i];
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
 for (j = 0; j < size; j++)
-str[i + j] = cp_2[j];
+str[i + j] = s2[j];
 str[i + j] = '\0';
 return (str);
 }
