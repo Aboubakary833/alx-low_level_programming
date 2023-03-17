7700beaed6149022cfe2f78ea36e7531e643dd5d
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-17 00:04:42 +0000

    done

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,11 @@
+/**
+ * _realloc - Reallocate memory
+ * @ptr: Pointer
+ * @old_size: Pointer old size
+ * @new_size: Pointer new size
+ * Return: reallocation address or NULL
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
