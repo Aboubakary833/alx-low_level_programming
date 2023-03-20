dacb14c00a71c42adff1031260ad2583cc2d8765
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-20 21:29:42 +0000

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,11 @@
 #define _DOG_H
 
 /**
- * dog - A structure for a dog
+ * struct dog - dog
  * @name: The name
  * @age: The age
  * @owner: The owner
+ * Description: A structure for a dog
 */
 struct dog
 {
@@ -15,14 +16,9 @@ char *owner;
 };
 
 /**
- * Type definition for dogs
+ * dog_t - Type definition for dogs
 */
 typedef struct dog dog_t;
-
-/**
- * Dog functions prototypes
-*/
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
