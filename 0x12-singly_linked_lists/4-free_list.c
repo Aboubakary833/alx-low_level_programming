026e96b30f6401a2258d4e2a174182a69d063797
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-03-30 19:34:48 +0000

    done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Free allocated memory of a list
+ * @head: List head's pointer
+ * Return: void
+*/
+void free_list(list_t *head)
+{
+list_t *next;
+while (head)
+{
+if (head->next)
+{
+next = head->next;
+free(head->str);
+free(head);
+}
+free_list(next);
+}
+}
