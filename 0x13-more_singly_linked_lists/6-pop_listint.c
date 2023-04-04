c15d8566275b5275e66ced62d90bee8b101ab473
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-04 17:14:06 +0000

    done

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - Pop the head from the list
+ * @head: The head
+ * Return: The value of the head on Success
+*/
+int pop_listint(listint_t **head)
+{
+int n;
+if ((*head) == NULL)
+{
+	return (0);
+}
+n = (*head)->n;
+free((*head));
+return (n);
+}
