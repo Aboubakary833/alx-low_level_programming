07cadf4f4d8e1f792f5bf0707220636eda000f44
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-04 17:36:46 +0000

    done

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,14 @@
 int pop_listint(listint_t **head)
 {
 int n;
+listint_t *next;
 if (head == NULL)
 {
 	return (0);
 }
 n = (*head)->n;
+next = (*head)->next;
 free((*head));
+(*head) = next;
 return (n);
 }
