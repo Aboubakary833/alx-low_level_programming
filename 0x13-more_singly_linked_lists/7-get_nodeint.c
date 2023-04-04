a0fc06f3aff56a47a6504cee5c9e87a3075d2a07
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-04 19:22:56 +0000

    done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 listint_t *prev, *first, *found = NULL;
 unsigned int i;
-if (head != NULL && index != 0)
+if (head && index)
 {
 	i = 0;
 	first = head;
