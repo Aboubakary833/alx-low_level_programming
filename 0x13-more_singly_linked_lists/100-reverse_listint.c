fd8612cced964df5d20cef9926f5eee71b8d4673
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-05 19:31:48 +0000

    done

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverse a int list
+ * @head: The list head
+ * Return: Pointer to the first head
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
