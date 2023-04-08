e084407eea2539ade70c026c10c6def584c7b853
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-08 17:51:39 +0000

    (done): binary to unsigned int

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_bit - Get bit at a given index of number's binary rep
+ * @n: The number
+ * @index: Index of the bit
+ * Return: the bit or -1
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result = -1;
+	while (n)
+	{
+		if (index == 0)
+		{
+			result = (n & 1);
+			break;
+		}
+		n >>= 1;
+		index--;
+	}
+	return (result);
+}
