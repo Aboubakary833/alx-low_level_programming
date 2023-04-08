2c54a7067a6ee336d6fd1e0b50708efcdbf08592
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-08 17:52:06 +0000

    (done)

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,8 +4,17 @@
  * reverse_listint - Reverse a int list
  * @head: The list head
  * Return: Pointer to the first head
-*/
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-
+	listint_t *cur, *next;
+	while ((*head) != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = cur;
+		cur = (*head);
+		(*head) = next;
+	}
+	(*head) = cur;
+	return (*head);
 }
