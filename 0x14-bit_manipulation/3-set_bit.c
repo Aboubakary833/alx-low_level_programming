3df0affe14dcd41125e102ba4b2dfcfcbc431475
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-08 18:54:08 +0000

    (done)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,13 +3,13 @@
  * @n: The number to manipulate
  * @index: Bit's index
  * Return: 1 on Success or -1 on Error
-*/
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
-	(*n) = (*n) | (index >> 1);
+	(*n) = (*n) | (1 << index);
 	return (1);
 }
