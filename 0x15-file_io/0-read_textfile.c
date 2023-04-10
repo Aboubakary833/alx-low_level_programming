b64959c29c3c47b4b991f776ebc2547bcdd1fd6a
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-10 13:29:15 +0000

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,28 +10,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buffer;
-ssize_t readCount, totalCount;
+	int fd;
+	char *buffer;
+	ssize_t readCount, totalCount;
 
-if (filename == NULL)
-	return (0);
+	if (filename == NULL)
+		return (0);
 
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-	return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-	return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-readCount = read(fd, buffer, letters);
-totalCount = write(STDOUT_FILENO, buffer, letters);
-if (readCount == -1 || totalCount == -1)
-	return (0);
+	readCount = read(fd, buffer, letters);
+	totalCount = write(STDOUT_FILENO, buffer, letters);
+	if (readCount == -1 || totalCount == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-free(buffer);
-close(fd);
+	free(buffer);
+	close(fd);
 
-return (totalCount);
+	return (totalCount);
 }
