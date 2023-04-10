a50379c7e7fdc11762e85b267e16738ae77da1b5
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-10 13:34:23 +0000

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	readCount = read(fd, buffer, letters);
-	totalCount = write(STDOUT_FILENO, buffer, letters);
+	totalCount = write(STDOUT_FILENO, buffer, readCount);
 	if (readCount == -1 || totalCount == -1 || readCount != totalCount)
 	{
 		free(buffer);
