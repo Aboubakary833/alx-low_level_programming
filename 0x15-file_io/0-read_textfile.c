809ac31e4acbd2764766645beb900cc1127dfa0a
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-10 12:57:16 +0000

    0-read_textfile.c done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read a text file
+ * @filename: The file name
+ * @letters: Number of letters to read
+ * Return: File letters count or 0 on Error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+char *buffer;
+ssize_t readCount, totalCount;
+buffer = malloc(letters);
+if (buffer == NULL)
+	return (0);
+
+if (filename == NULL)
+	return (0);
+
+fd = open(filename, O_RDONLY);
+
+if (fd == -1)
+	return (0);
+
+readCount = read(fd, buffer, letters);
+
+if (readCount == -1)
+	return (0);
+
+totalCount = write(STDOUT_FILENO, buffer, letters);
+
+if (totalCount == -1)
+	return (0);
+
+return (readCount);
+}
