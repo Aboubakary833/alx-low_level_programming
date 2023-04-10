d20ed2e42bc75dd922653839c6d489f47c8e99ec
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-10 15:07:53 +0000

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,26 +6,26 @@
  * @filename: The file name
  * @text_content: The content of the file
  * Return: 1 on Success or -1 on Error
-*/
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-	if (fd != -1 && text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
-	else if (fd == -1)
-		return (-1);
-	while (text_content[i] != '\0')
-		i++;
-	
-	fd = write(fd, text_content, i);
-	if (fd == -1)
-		return (-1);
+int fd, i = 0;
+if (!filename)
+	return (-1);
+fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+if (fd != -1 && text_content == NULL)
+{
 	close(fd);
 	return (1);
+}
+else if (fd == -1)
+	return (-1);
+while (text_content[i] != '\0')
+	i++;
+
+fd = write(fd, text_content, i);
+if (fd == -1)
+	return (-1);
+close(fd);
+return (1);
 }
