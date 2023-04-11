600c255e3873a7d25e0565ab9ad2fd1e53b0a031
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-11 15:26:36 +0000

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@
  */
 int main(int argc, char *argv[])
 {
-int f_fd, s_fd, char_count = 0, read_count, write_count;
+int f_fd, s_fd, char_count, write_count;
 char *from, *to, *buffer;
 if (argc != 3)
 {
@@ -25,10 +25,9 @@ to = argv[2];
 buffer = create_buffer(from);
 f_fd = open(from, O_RDONLY);
 s_fd = open(to, O_WRONLY | O_TRUNC | O_CREAT);
-read_count = read(f_fd, buffer, sizeof(char) * 1024);
 if (s_fd == -1)
 	s_fd = creat(to, 0664);
-if (f_fd == -1 || read_count == -1)
+if (f_fd == -1)
 {
 	free(buffer);
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
@@ -41,6 +40,9 @@ else if (s_fd == -1)
 	exit(99);
 }
 fchmod(s_fd, 0664);
+while (read(f_fd, buffer, sizeof(char) * 1024) > 0)
+{
+char_count = 0;
 while (buffer[char_count] != '\0')
 	char_count++;
 write_count = write(s_fd, buffer, char_count);
@@ -49,6 +51,7 @@ if (write_count == -1)
 	free(buffer);
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
 	exit(99);
+}
 }
 close_fd(f_fd);
 return (0);
