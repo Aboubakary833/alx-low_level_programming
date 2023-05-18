57d238a9c133e9e6d225f2e125f5e3fafa238c1d
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-18 22:47:10 +0000

    done: 6-sum_dlistint

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Insert a node at dlistint at a given index
+ * @h: dlistint head
+ * @idx: The index
+ * @n: The value of the node
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *new, *copy;
+unsigned int i = 0, len = 0;
+if (idx == 0)
+	return (add_dnodeint(h, n));
+copy = (*h);
+while (copy)
+{
+	len++;
+	copy = copy->next;
+}
+if (idx == len - 1)
+	return (add_dnodeint_end(h, n));
+free(copy);
+copy = (*h);
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+	return (NULL);
+new->n = n;
+while (copy != NULL)
+{
+	if (idx == i)
+	{
+		new->prev = copy->prev;
+		new->next = copy;
+		*h = new;
+	}
+	copy = copy->next;
+	i++;
+}
+
+return (new);
+}
