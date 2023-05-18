b143bde2929ef89934dd083819805a589b4c15d0
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-18 14:11:06 +0000

    fix: Betty error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,13 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *copy = h;
-	int _size = 0;
-	while (copy)
-	{
-		printf("%d\n", copy->n);
-		_size++;
-		copy = copy->next;
-	}
-	return ((size_t)_size);
+const dlistint_t *copy = h;
+int _size = 0;
+while (copy)
+{
+	printf("%d\n", copy->n);
+	_size++;
+	copy = copy->next;
+}
+return ((size_t)_size);
 }
