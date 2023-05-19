60cd76d01c2669a0c4877a9fac053cf6235a33b0
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-19 12:28:20 +0000

    done: 8-delete_dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,40 +9,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new;
-dlistint_t *copy;
-unsigned int i;
-
-new = NULL;
+dlistint_t *new, *copy;
+unsigned int i = 0, len = 0;
 if (idx == 0)
-	new = add_dnodeint(h, n);
-copy = *h;
-i = 1;
+	return (add_dnodeint(h, n));
+copy = (*h);
+while (copy)
+{
+	len++;
+	copy = copy->next;
+}
+if (idx == len)
+	return (add_dnodeint_end(h, n));
+else if (idx > len)
+	return (NULL);
+copy = (*h);
 if (copy != NULL)
 	while (copy->prev != NULL)
 		copy = copy->prev;
 while (copy != NULL)
 {
-	if (i == idx)
+	if ((i + 1) == idx)
 	{
-		if (copy->next == NULL)
-			new = add_dnodeint_end(h, n);
-		else
-		{
-			new = malloc(sizeof(dlistint_t));
-			if (new != NULL)
-			{
-				new->n = n;
-				new->next = copy->next;
-				new->prev = copy;
-				copy->next->prev = new;
-				copy->next = new;
-			}
-		}
-		break;
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = copy->next;
+		new->prev = copy;
+		copy->next = new;
+		return (new);
 	}
 	copy = copy->next;
 	i++;
 }
-return (new);
+	return (NULL);
 }
