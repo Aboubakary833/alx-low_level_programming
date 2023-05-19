2c7af2551bf403e0f987705c9d02024aa1c0decb
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-19 19:35:05 +0000

    done: 8-delete_dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,43 +4,48 @@
  * @head: Double linked list head
  * @index: Index of node to delete
  * Return: 1 if successed or -1 if it failed
-*/
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *copy, *temp;
-unsigned int i = 1;
-copy = (*head);
-if (copy == NULL)
-	return (-1);
-while (copy->prev != NULL)
-	copy = copy->prev;
-if (index == 0)
-{
-	temp = (*head)->next;
-	free((*head));
-	(*head) = temp;
-	if ((*head) != NULL)
-		(*head)->prev = NULL;
-	return (1);
-}
-while (copy)
-{
-	if (index == i - 1)
+	dlistint_t *copy, *temp;
+	unsigned int i = 1;
+	copy = (*head);
+	if (copy == NULL)
+		return (-1);
+	while (copy->prev != NULL)
+		copy = copy->prev;
+	if (index == 0)
 	{
-		temp = copy->prev;
-		if (temp)
-			temp->next = copy->next;
-		else
+		if (copy->next)
 		{
-			temp = copy->next;
-			temp->prev = copy->prev;
+			*head = copy->next;
+			(*head)->prev = NULL;
 		}
+		else
+			*head = NULL;
+
 		free(copy);
-		copy = temp;
 		return (1);
 	}
-	i++;
-	copy = copy->next;
-}
-return (-1);
+
+	while (copy)
+	{
+		if (index == i - 1)
+		{
+			temp = copy->prev;
+			if (temp)
+				temp->next = copy->next;
+			else
+			{
+				temp = copy->next;
+				temp->prev = copy->prev;
+			}
+			free(copy);
+			copy = temp;
+			return (1);
+		}
+		i++;
+		copy = copy->next;
+	}
+	return (-1);
 }
