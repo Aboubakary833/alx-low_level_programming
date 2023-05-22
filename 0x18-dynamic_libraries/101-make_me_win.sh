1027b3c3a8e42391f3e7e7304617efdf2326b901
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-22 15:23:53 +0000

    done: gm

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LD_PRELOAD=https://github.com/Aboubakary833/alx-low_level_programming/tree/main/0x18-dynamic_libraries/gm.so ./gim
+LD_PRELOAD=https://github.com/Aboubakary833/alx-low_level_programming/tree/main/0x18-dynamic_libraries/gm.so ./gm
