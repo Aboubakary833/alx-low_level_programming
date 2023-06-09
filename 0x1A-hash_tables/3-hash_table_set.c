50ecf1ca99ae6d4d21d8d0e0d51507f2734f86a8
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-09 17:55:43 +0000

    added: hash_table_set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Set a new hash_node
+ * @ht: The Hash Table
+ * @key: The node key
+ * @value: The node value
+ * Return: 1 or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+hash_node_t *newNode;
+unsigned long int i, idx;
+if (ht == NULL || key == NULL || strcmp(key, "") || value == NULL)
+	return (0);
+idx = key_index((const unsigned char *)key, ht->size);
+for (i = idx; ht->array[i]; i++)
+{
+	if (strcmp(ht->array[i]->key, key) == 0)
+	{
+		free(ht->array[i]->value);
+		ht->array[i]->value = (char *)value;
+		return (1);
+	}
+}
+newNode = malloc(sizeof(hash_node_t));
+if (!newNode)
+	return (0);
+newNode->key = (char *)key;
+if (newNode->key == NULL)
+{
+	free(newNode);
+	return (0);
+}
+newNode->value = (char *)value;
+newNode->next = ht->array[idx];
+ht->array[idx] = newNode;
+
+return (1);
+}
