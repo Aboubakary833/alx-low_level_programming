c8beed89cf91bc779a5ff548d907c442e65ac05b
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-09 15:55:03 +0000

    added: hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i;
 hash_table_t *newHashTable = malloc(sizeof(hash_table_t));
-hash_node_t *hashTableArray = malloc(sizeof(hash_node_t) * size);
+hash_node_t **hashTableArray = malloc(sizeof(hash_node_t *) * size);
 if (!newHashTable || !hashTableArray)
 	return (NULL);
 
 newHashTable->size = size;
-newHashTable->array = &hashTableArray;
+newHashTable->array = hashTableArray;
 for (i = 0; i < size; i++)
 	newHashTable->array[i] = NULL;
 return (newHashTable);
